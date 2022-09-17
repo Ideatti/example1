88203b90654b0ae1d6ee77ca399e240993bc166b
Tomppa <tompadc@hotmail.com>
2022-09-17 14:27:33 +0300

    hello function works

--- eka.js
+++ eka.js
@@ -1,1 +1,6 @@
-console.log("Eka sovellus"); 
+console.log("Eka sovellus");
+console.log("Heissuli vei");
+for(var x= 1; x<5; x++){
+    console.log("Rivi "+x);
+}
+console.log("Loopin jälkeen x="+x);
